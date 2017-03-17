3a4a1939eb0a820fd5ab00f6ff8faf2fd12d9944
vasudev <vasu123689@gmail.com>
2017-03-17 12:27:23 -0800

    main method in separate class

---
+++ src/MobileTest.java
@@ -0,0 +1,16 @@
+
+public class MobileTest {
+
+	public static void main(String[] args){
+		
+		
+	Mobile details = new Mobile("Apple","IOS","6S",800);
+	
+	System.out.println("Cost In dollars:"+ " " +details.getCost());
+	
+	System.out.println("Model Is:"+ " " +details.model);
+		
+	}
+	
+	
+}
